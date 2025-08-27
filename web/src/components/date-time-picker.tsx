098b574cc18198c8e69f026acc4474e324b1de4d
kuaifan <aipaw@live.cn>
2025-08-27 20:01:52 +0800

    no message

--- web/src/components/date-time-picker.tsx
+++ web/src/components/date-time-picker.tsx
@@ -299,12 +299,12 @@ function Calendar({
         day: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 rounded-1',
         day_button: cn(
           buttonVariants({ variant: 'ghost' }),
-          'h-9 w-9 p-0 font-normal aria-selected:opacity-100 rounded-l-md rounded-r-md',
+          'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
         ),
         range_end: 'day-range-end',
         selected:
           'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground rounded-l-md rounded-r-md',
-        today: 'bg-accent text-accent-foreground',
+        today: 'bg-accent text-accent-foreground rounded-l-md rounded-r-md',
         outside:
           'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
         disabled: 'text-muted-foreground opacity-50',
